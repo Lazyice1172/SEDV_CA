800c028506eaf75215d30d026aab7cf6f7532e01
ChrisVar95 <christina.vargka@gmail.com>
2022-04-20 14:09:49 +0100

    backup

---
+++ CA/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.utils.translation import gettext_lazy as _
+from django.shortcuts import render
+
+
+def testlang(request):
+    return HttpResponse(_('Welcome to language translation!'))
+
+
+def index(request):
+    return render(request, "index.html")
